c9b45940b63987f49dc95f9a8d43be3fdd9f91cd
Zafeer Abbasi <zafeer.abbasi@xantrex.com>
2025-04-10 14:48:18 -0700

    Ignore specific files in Git

--- Projects/VSCode/000_Testing_FreeRTOS/build/Release/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
-  file(REMOVE_RECURSE
-  "0000_Testing_FreeRTOS.map"
-  )
-endif()
